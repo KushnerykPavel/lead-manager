de372a3d68bcf7f949599f2d2091fa776b511a72
KushnerykPavel <levelup.kpi@gmail.com>
2019-05-13 19:43:35 +0300

    finish add alerts

--- leadmanager/frontend/src/reducers/index.js
+++ leadmanager/frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import leads from '../coomponents/leads/leads';
 import errors from './errors';
+import messages from './messages';
 
 export default combineReducers({
+    messages,
     errors,
     leads
 });
